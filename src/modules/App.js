bfbc851490045903fa4b86fa58991077fb3951f6
MechaDragonX <theofficialrv@gmail.com>
2020-06-10 22:11:48 -0700

    Fix issues in App.js for Trade page

--- src/modules/App.js
+++ src/modules/App.js
@@ -6,6 +6,7 @@ import HomePage from './homepage/Homepage';
 import SignupPage from './loginpage/SignPage';
 import CreateGroupPage from './accountpage/CreateGroupPage';
 import Listing from './homepage/Listing';
+import Trade from './homepage/Trade';
 import useSticky from '../miscJS/useSticky';
 import '../css/App.css';
 
@@ -50,7 +51,7 @@ function App() {
           <Route exact={true} path="/login" render={()=><LoginPage userInfo={userInfo} setUserInfo={info=>setUserInfo(info)}/>}/>
           <Route exact={true} path="/group/:groupId" render={match=><GroupDetailPage userInfo={userInfo} match={match} setUserInfo={info=>setUserInfo(info)}/>}/>
           <Route exact={true} path="/item/:groupId/:postId" render={match=><Listing match={match}/>}/>
-          <Route exact={true} path="/trade/:groupId1/:postId1/groupId2/:postId2" render={match=><Listing match={match}/>}/>
+          <Route exact={true} path="/trade/:groupId1/:postId1/groupId2/:postId2" render={match=><Trade match={match}/>}/>
           <Route exact={true} path="/test" render={()=><h1>test</h1>}/>
         </main>
       </Router>
