5fadaf00d64a114ee708d41dd8374ad1849211bf
MechaDragonX <theofficialrv@gmail.com>
2020-06-10 21:26:44 -0700

    Make them 2 space indent

--- src/modules/homepage/Trade.js
+++ src/modules/homepage/Trade.js
@@ -2,41 +2,42 @@ import React from 'react';
 import './../../css/App.css';
 import './../../css/Trade.css';
 
+const fetch = require('node-fetch');
+
 class Trade extends React.Component {
-    render() {
-        return (
+  render() {
+    return (
+      <div className='grid-container'>
+        <div className='leftItem'>
+          <h3>Raghav Vivek</h3>
+          <div className='card'>
+            <p>Yarn Yoshi Amiibo</p>
+            <img className='thumbnail'
+              src='https://nintendofigures.files.wordpress.com/2017/02/yarn-poochy-amiibo-licking-yarn-yoshi.jpg'
+            />
+            <p>Looking for: Other rare amiibo</p>
+          </div>
+        </div>
 
-            <div className='grid-container'>
-                <div className='leftItem'>
-                    <h3>Raghav Vivek</h3>
-                    <div className='card'>
-                        <p>Yarn Yoshi Amiibo</p>
-                        <img className='thumbnail'
-                            src='https://nintendofigures.files.wordpress.com/2017/02/yarn-poochy-amiibo-licking-yarn-yoshi.jpg'
-                        />
-                        <p>Looking for: Other rare amiibo</p>
-                    </div>
-                </div>
+        <div className='controls'>
+          <input type='checkbox' title='Ready?' />
+          <button>Trade!</button>
+          <input type='checkbox' title='Ready?' />
+        </div>
 
-                <div className='controls'>
-                    <input type='checkbox' title='Ready?' />
-                    <button>Trade!</button>
-                    <input type='checkbox' title='Ready?' />
-                </div>
-
-                <div className='rightItem'>
-                    <h3>Yifan Xu</h3>
-                    <div className='card'>
-                        <p>Celica Amiibo</p>
-                        <img className='thumbnail'
-                            src='https://pbs.twimg.com/media/EOQc9duW4AIaEs5.png'
-                        />
-                        <p>Looking for: Other rare amiibo</p>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+        <div className='rightItem'>
+          <h3>Yifan Xu</h3>
+          <div className='card'>
+            <p>Celica Amiibo</p>
+            <img className='thumbnail'
+              src='https://pbs.twimg.com/media/EOQc9duW4AIaEs5.png'
+            />
+            <p>Looking for: Other rare amiibo</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Trade;
